12c123451a3259534b9f2eeca5b4cc2241e76c2d
Julio Rennan <julio.rennann@gmail.com>
2022-12-09 02:02:54 -0400

    initial commit

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export default {
-  secret: 'some-secret-shit-goes-here',
-  refreshTokenSecret: 'some-secret-refresh-token-shit',
+  secret: 'some-secret-goes-here',
+  refreshTokenSecret: 'some-secret-refresh-token',
   port: 3000,
   tokenLife: 900,
   refreshTokenLife: 86400,
